ef9bedb6f7a91b0033a963b1c58cc92a308e3715
Gary Broderick <garybroderick82@gmail.com>
2024-07-10 11:06:59 +0000

    Tidy up code and remove empty rows printed to terminal

--- run.py
+++ run.py
@@ -172,7 +172,7 @@ def update_monthly_worksheet():
         else:
             print(f"Error: Month {month_year} not found in progress sheet.")
             
-    print("Monthly worksheet updated successfully.\n")
+    print("Monthly worksheet updated successfully.")
 
 
 def calculate_project_payback():
@@ -346,53 +346,6 @@ def display_project_data(data):
         return 'main_menu'
 
 
-"""def update_monthly_worksheet(data):
-    """
-    #Update monthly worksheet, add new row with the list data provided
-"""
-    print("Updating monthly worksheet...\n")
-    monthly_worksheet = SHEET.worksheet("monthly")
-    monthly_worksheet.append_row(data)
-    print("Monthly worksheet updated successfully.\n")
-
-
-def calculate_monthly_worksheet(daily_row):
-    """
-    #Calculate monthly energy use
-"""
-    print("Calculating monthly data...\n")
-    
-    monthly = SHEET.worksheet("monthly").get_all_values()
-    monthly_row = monthly[-1]
-    new_monthly_row = monthly_row[1:]
-    new_daily_row = daily_row[1:]
-
-    monthly_data = []
-    for month, day in zip(new_monthly_row, new_daily_row):
-        month_data = int(month) + day
-        monthly_data.append(month_data)
-    
-    return monthly_data
-
-
-def calculate_monthly_savings(month_row):
-    """
-    #Calculate monthly savings
-"""
-    print("Calculating monthly savings...\n")
-    monthly = SHEET.worksheet("monthly").get_all_values()
-    monthly_row = monthly[-1]
-    str_month = monthly_row[1:]
-    monthly_num = [int(value) for value in str_month]
-    consumed = monthly_num[0]
-    exported = monthly_num[1]
-    imported = monthly_num[2]
-    
-    grid = (consumed - imported) * 0.2287
-    saving = grid + (exported * 0.24)
-    print(f"This months's savings:  €{saving}")"""
-
-
 def print_menu():
     """
     Print the main menu options.
@@ -464,26 +417,5 @@ def main():
         else:
             print("Invalid choice. Please choose 1, 2, 3, or 4.")
 
-        print()  # Add a newline before the main menu
-
     
-    """
-    daily_energy_data = get_daily_data()
-    str_list = daily_energy_data[1:]
-    num_list = [float(value) for value in str_list]
-    new_daily_list = []
-    new_daily_list.append(daily_energy_data[0])
-    new_daily_list.extend(num_list)
-    update_daily_worksheet(new_daily_list)
-    update_monthly_worksheet()
-    #new_monthly_data = update_monthly_worksheet(new_daily_list)
-    #monthly_list = []
-    #monthly_list.append(daily_energy_data[0])
-    #monthly_list.extend(new_monthly_data)
-    #update_monthly_worksheet(monthly_list)
-    #calculate_monthly_savings(monthly_list)
-    """
-    
-
-#print("Welcome to Solar System Data Automation")
 main()
